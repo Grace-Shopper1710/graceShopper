eec8f27aeee6862ba38e1fd3d292b9612d33ea60
Julia Jaspers <juliajaspers@Julias-MacBook-Pro.local>
2018-01-09 10:53:41 -0500

    Added backend routes for products, breweries, styles, users, and orders ;

---
+++ server/api/orders.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../db/models');
+const Order = models.Order;
+
+module.exports = router;
+
+router.get('/', (req, res, next) => {
+    Order.findAll({
+        include: [{all: true}]
+    })
+    .then(orders => res.json(orders))
+    .catch(next)
+})
+
+router.get('/:orderId', (req, res, next) => {
+    Order.findById(req.params.orderId,
+        {include: [{all: true}]
+    })
+    .then(order => res.json(order))
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Order.create(req.body)
+    .then(order => res.json(order))
+    .catch(next)
+})
+
+router.put('/:orderId', (req, res, next) => {
+    Order.findOne({
+        where: {id: req.params.orderId}
+    })
+    .then(order => order.update(req.body))
+    .then(order => res.json(order))
+    .catch(next)
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    Order.destroy({
+        where: {id: req.params.orderId}
+    })
+    .then(() => res.send("Successful deletion"))
+    .catch(next)
+})
---
+++ server/api/styles.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../db/models');
+const Style = models.Style;
+
+module.exports = router;
+
+router.get('/', (req, res, next) => {
+    Style.findAll({
+        include: [{all: true}]
+    })
+    .then(styles => res.json(styles))
+    .catch(next)
+})
+
+router.get('/:styleId', (req, res, next) => {
+    Style.findById(req.params.styleId,
+        {include: [{all: true}]
+    })
+    .then(style => res.json(style))
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Style.create(req.body)
+    .then(style => res.json(style))
+    .catch(next)
+})
+
+router.put('/:styleId', (req, res, next) => {
+    Style.findOne({
+        where: {id: req.params.styleId}
+    })
+    .then(style => style.update(req.body))
+    .then(style => res.json(style))
+    .catch(next)
+})
+
+router.delete('/:styleId', (req, res, next) => {
+    Style.destroy({
+        where: {id: req.params.styleId}
+    })
+    .then(() => res.send("Successful deletion"))
+    .catch(next)
+})
