c7108e514714161ce4f5961131b610cbce65d9d5
LyssaStiller <stiller108@gmail.com>
2018-01-09 15:21:15 -0500

    updated Orders Model

--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -2,10 +2,67 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Orders = db.define('orders', {
+  id: {
+    type: Sequelize.INTEGER
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  products: {
+    type: Sequelize.ARRAY
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  zipCode: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  creditCard: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  creditCardExpDate: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  //VIRTUALS
+
+  fullName: {
+    type: Sequelize.VIRTUAL,
+    get () {
+      return this.getDataValue('firstName') + ' ' + this.getDataValue('lastName')
+    }
+  },
   shippingAddress: {
-      type: Sequelize.STRING,
-      allowNull: false
+    type: Sequelize.VIRTUAL,
+    get() {
+      return this.getDataValue('address') + ' ' + this.getDataValue('city') + ' ' + this.getDataValue('state') + this.getDataValue('zipcode')
+    }
   }
 })
+
+Orders.prototype.total = function(){
+  let total = 0
+  for(var i = 0; i < this.products.length; i++){
+    total += this.products[i].price
+  }
+  return total
+}
 
 module.exports = Orders
--- server/db/models/reviews.js
+++ server/db/models/reviews.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Reviews = db.define('reviews', {
+  id: {
+    type: Sequelize.INTEGER,
+  },
   content: {
     type: Sequelize.TEXT
   }
 })
 
-module.exports = Revies
+module.exports = Reviews
