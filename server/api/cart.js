1d47a99f3a180af71b18a7f1dff7e1599f7e2def
Ko-Hsin Chu <chukohsin@gmail.com>
2018-01-10 14:34:55 -0500

    changed post/delete to put

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,28 +4,28 @@ const { Product } = require('../db/models')
 
 module.exports = router
 
-//const newCart = { items: [], totals: 0 }
+//const newCart = { products: [], totals: 0 }
 
 //Where should we form a new cart?
 //- when user add first item in the cart?
 
 router.use((req, res, next) => {
-	if (!req.session.cart) req.session.cart = { items: [], totals: 0 }
+	if (!req.session.cart) req.session.cart = { products: [], totals: 0 }
 	next()
 })
 
 router.get('/', (req, res, next) => {
-	let cart = req.session.cart.items.length ? req.session.cart : false
+	let cart = req.session.cart.products.length ? req.session.cart : false
 	res.json(cart)
 })
 
-router.delete('/:productId', (req, res, next) => {
+router.put('/:productId', (req, res, next) => {
 	removeFromCart(req.params.productId, req)
 	res.send(req.session.cart)
 })
 
 //Add to cart
-router.post('/', (req, res, next) => {
+router.put('/', (req, res, next) => {
 	let newItem  = { id: req.body.productId, qty: req.body.qty }
 	if (inCart(newItem.id, req)) {
 		updateCart(newItem.id, newItem.qty, req)
@@ -48,13 +48,13 @@ router.post('/', (req, res, next) => {
 
 //Check whther this item is already in cart
 function inCart(productId, req) {
-	return req.session.cart.items.reduce((accu, curr) => {
+	return req.session.cart.products.reduce((accu, curr) => {
 		return curr.id === productId || accu
 	}, false)
 }
 
 function updateCart(productId, qty, req) {
-	req.session.cart.items.forEach(el => {
+	req.session.cart.products.forEach(el => {
 		if (el.id === productId) {
 			el.qty  = el.qty + qty
 		}
@@ -62,19 +62,19 @@ function updateCart(productId, qty, req) {
 }
 
 function addToCart(newItem, req) {
-	req.session.cart.items.push(newItem)
+	req.session.cart.products.push(newItem)
 }
 
 function removeFromCart(productId, req) {
-	req.session.cart.items.forEach((el, i) => {
+	req.session.cart.products.forEach((el, i) => {
 		if (el.id === productId) {
-			req.session.cart.items.splice(i, 1)
+			req.session.cart.products.splice(i, 1)
 		}
 	})
 }
 
 // function calculateTotals(req) {
-// 	 let totals = req.session.cart.items.reduce((accu, curr) => {
+// 	 let totals = req.session.cart.products.reduce((accu, curr) => {
 // 		Product.findOne({
 // 			where: {
 // 				id: curr.id
