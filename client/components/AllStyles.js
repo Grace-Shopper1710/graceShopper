82730b017034e30cf74d02afa2eda11464d1ecdc
Sara Rose Gallagher <srgallag@gmail.com>
2018-01-14 14:23:58 -0500

    added back missing li

--- client/components/AllStyles.js
+++ client/components/AllStyles.js
@@ -3,32 +3,33 @@ import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import store from '../store'
 
-const mapStateToProps = state => ({styles: state.style, user: state.user})
+const mapStateToProps = state => ({ styles: state.style, user: state.user })
 
 export class AllStyles extends React.Component {
 
-  render(){
+  render() {
 
-    const {styles, user} = this.props
+    const { styles, user } = this.props
     const isAdmin = user ? user.isAdmin : false
 
-    return(
+    return (
       <div className="allStyles">
         {isAdmin && <NavLink to={'/admin/newstyle'}><button>Create a New Style</button></NavLink>}
         <ul>
           {
             styles.map(style => (
               <li key={style.id}>
-              {
-									isAdmin &&
-									<div>
-										<NavLink to={`/style/${style.id}/edit`}><button>Edit!</button></NavLink>
-									</div>
-								}
+                {
+                  isAdmin &&
+                  <div>
+                    <NavLink to={`/style/${style.id}/edit`}><button>Edit!</button></NavLink>
+                  </div>
+                }
                 <div>
                   <NavLink to={`/styles/${style.id}`}>{style.name}</NavLink>
                   <p>{style.description}</p>
                 </div>
+              </li>
             ))
           }
         </ul>
