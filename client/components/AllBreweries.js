4293f67543bd478a63e9b155ab5751b72028178b
Sara Rose Gallagher <srgallag@gmail.com>
2018-01-16 13:00:28 -0500

    password strength plus...

--- client/components/AllBreweries.js
+++ client/components/AllBreweries.js
@@ -5,29 +5,30 @@ import { NavLink } from 'react-router-dom'
 const mapStateToProps = state => ({ breweries: state.brewery, user: state.user })
 
 const AllBreweries = (props) => {
-		const { breweries, user } = props
-		const isAdmin = user ? user.isAdmin : false
-		return (
-			<div>
-				{isAdmin && <NavLink to={'/admin/newbrewery'}><button className="btn btn-primary btn-sm">Add a New Brewery</button></NavLink>}
-					{
-						breweries.map(brewery => (
-							<div key={brewery.id}>
-								<img src={brewery.image} />
-								<div>
-									<NavLink to={`/breweries/${brewery.id}`}>{brewery.name}</NavLink>
-									{brewery.style ? brewery.style.name : null}<br />
-									{brewery.description}<br />
-								</div>
-								{
-								isAdmin &&
-								<NavLink to={`/breweries/${brewery.id}/edit`}><button className="btn btn-primary btn-sm">Edit</button></NavLink>
-								}
-							</div>
-						))
-					}
-			</div>
-		)
+	const { breweries, user } = props
+	const isAdmin = user ? user.isAdmin : false
+	return (
+		<div>
+			{isAdmin && <NavLink to={'/admin/newbrewery'}><button className="btn btn-primary btn-sm">Add a New Brewery</button></NavLink>}
+			{
+				breweries.map(brewery => (
+					<div key={brewery.id}>
+						<NavLink to={`/breweries/${brewery.id}`}><img src={brewery.image} /></NavLink>
+						
+						<div>
+							<NavLink to={`/breweries/${brewery.id}`}>{brewery.name}</NavLink>
+							{brewery.style ? brewery.style.name : null}<br />
+							{brewery.description}<br />
+						</div>
+						{
+							isAdmin &&
+							<NavLink to={`/breweries/${brewery.id}/edit`}><button className="btn btn-primary btn-sm">Edit</button></NavLink>
+						}
+					</div>
+				))
+			}
+		</div>
+	)
 }
 
 const allBreweriesContainer = connect(mapStateToProps)(AllBreweries)
